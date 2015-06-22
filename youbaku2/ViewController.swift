b4b6c062734ab80845e47239d90a9d29d6a6ae52
Beraat ALDEMİR (ULAKBİM) <beraat.aldemir@tubitak.gov.tr>
2015-06-22 12:01:00 +0300

    Initial Commit

---
+++ youbaku2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  youbaku2
+//
+//  Created by ULAKBIM on 22/06/15.
+//  Copyright (c) 2015 Beraat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
